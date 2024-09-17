71b5f7ac731056f40b6ef574b1ce58e21bdef08b
Andreas Maakestad <andremaa@stud.ntnu.no>
2024-09-17 11:35:05 +0200

    Added datafiltering based on location, quality flags and inclination angle

--- data_acquisition/dataHandling.py
+++ data_acquisition/dataHandling.py
@@ -13,8 +13,20 @@ password = "Terrengmodell69!"
 Dates format: "yyyymmdd"
 '''
 
-startDate = "20240101"
-endDate = "20240101"
+start_date = "20240101"
+end_date = "20240101"
 
-data_fetching(startDate, endDate, username, password)
+'''
+Defining spatial filter
+Max_lat: float, min_lat: float, max_lon: float, min_lon: float, inc_angle: float
+'''
+max_lat = 36
+min_lat = 28
+max_lon = 65
+min_lon = 54
+inc_angle = 65
+
+
+
+data_fetching(start_date, end_date, username, password, max_lat, min_lat, max_lon, min_lon, inc_angle)
 
--- data_acquisition/functions.py
+++ data_acquisition/functions.py
@@ -30,13 +30,28 @@ def create_dates_array(startDate: str, endDate: str):
     
     return dates
 
-'''This function downloads the data blabla'''
 
-def data_fetching(startDate: str, endDate: str, username: str, password: str):
+'''
+This function filters the data based on geographical location, quality flag and inicident angle 
+quality flags 2, 4, 5, 8, 16, and 17
+'''
+def data_filtering(df, max_lat: float, min_lat: float, max_lon: float, min_lon: float, inc_angle: float):
+    
+    df_filtered_spatial = df[(df["sp_lon"] >= min_lon) & (df["sp_lon"] <= max_lon) & (df["sp_lat"] >= min_lat) & (df["sp_lat"] <= max_lat)]
+    df_filtered_inclination = df_filtered_spatial[(df["sp_inc_angle"] <= inc_angle)]
+    df_filtered_qf = df_filtered_inclination[(df["quality_flags"] != 2) | (df["quality_flags"] != 4) | (df["quality_flags"] != 5) | (df["quality_flags"] != 8) |(df["quality_flags"] != 16) | (df["quality_flags"] != 17)]
+    
+    return df_filtered_qf
+
+
+
+'''This function downloads the data'''
+
+def data_fetching(startDate: str, endDate: str, username: str, password: str, max_lat: float, min_lat: float, max_lon: float, min_lon: float, inc_angle: float):
     dates = create_dates_array(startDate, endDate)
     
     # List of satellite identifiers
-    satellites = [f'cyg0{i}' for i in range(1, 9)]
+    satellites = [f'cyg0{i}' for i in range(1, 2)]
 
 # Iterate over each satellite and date
     for sat in tqdm(satellites):
@@ -48,17 +63,44 @@ def data_fetching(startDate: str, endDate: str, username: str, password: str):
                 # Attempt to open the dataset
                 dataset = open_url(url, session=setup_session(username, password), protocol='dap4')
 
-                # Fetch data from the dataset if it exists
-                ddm_snr = np.array(dataset['ddm_snr'][:, 0][:45]) 
-                sp_lon = np.array(dataset['sp_lon'][:, 0][:45])
-                sp_lat = np.array(dataset['sp_lat'][:, 0][:45])
+                # Fetch data from the dataset 
+                ddm_snr = np.array(dataset['ddm_snr'][:, 0]) 
+                sp_lon = np.array(dataset['sp_lon'][:, 0])
+                sp_lon[sp_lon > 180] -= 360 #Adjusting the longitude to the correct values for plotting
+                sp_lat = np.array(dataset['sp_lat'][:, 0])
+                gps_tx_power_db_w = np.array(dataset['gps_tx_power_db_w'][:, 0])
+                gps_ant_gain_db_i = np.array(dataset['gps_ant_gain_db_i'][:, 0])
+                sp_rx_gain = np.array(dataset['sp_rx_gain'][:, 0])
+                tx_to_sp_range = np.array(dataset['tx_to_sp_range'][:, 0])
+                rx_to_sp_range = np.array(dataset['rx_to_sp_range'][:, 0])
+                prn_code = np.array(dataset['prn_code'][:, 0])
+                sp_inc_angle = np.array(dataset['sp_inc_angle'][:, 0]) 
+                quality_flags = np.array(dataset['quality_flags'][:, 0])
 
                 # Create a dataframe with the data
-                df = pd.DataFrame(sp_lon, columns=['sp_lon'])
-                df['sp_lat'] = sp_lat
-                df['ddm_snr'] = ddm_snr
+                
+                df = pd.DataFrame({
+                    'sp_lon': sp_lon,
+                    'sp_lat': sp_lat,
+                    'ddm_snr': ddm_snr,
+                    'gps_tx_power_db_w': gps_tx_power_db_w,
+                    'gps_ant_gain_db_i': gps_ant_gain_db_i,
+                    'sp_rx_gain': sp_rx_gain,
+                    'tx_to_sp_range': tx_to_sp_range,
+                    'rx_to_sp_range': rx_to_sp_range,
+                    'prn_code': prn_code,
+                    'sp_inc_angle': sp_inc_angle,
+                    'quality_flags': quality_flags
+                })                
+                '''
+                Filter parameters
+                Max_lat: float, min_lat: float, max_lon: float, min_lon: float, inc_angle: float
+                '''
+                df_filtered = data_filtering(df, max_lat, min_lat, max_lon, min_lon, inc_angle)
+                #Printing 45 first rows to check
+                print(df_filtered.head(10))
                 print(f"Data for {sat} on {date}:")
-                print(df)
+                
 
             except HTTPError as e:
                 # If the URL is invalid (404 error), print a message and skip this satellite/date
