e3d04a7f155ac10f562ab0e892ddb143964c8e27
Ole <oleevca@gmail.com>
2024-10-07 18:37:33 +0200

    Calculated for september 2023

--- dataHandling/fetchData.py
+++ dataHandling/fetchData.py
@@ -4,12 +4,6 @@ from functions import data_fetching
 username = "andreasmaakes"
 password = "Terrengmodell69!"
 
-'''
-Dates format: "yyyymmdd"
-'''
-
-start_date = "20231212"
-end_date = "20231213" 
 
 '''
 Defining spatial filter
@@ -25,13 +19,23 @@ min_lon = 54
 inc_angle = 65
 '''
 
-#Chad spatial filter
+'''
+Dates format: "yyyymmdd"
+'''
 
+start_date = "20230912"
+end_date = "20230913" 
+
+#Region name that is to be used for naming data folders
 name = "Chad"
+
+#Chad spatial filter
 min_lat = 10
 max_lat = 15
 min_lon = 16
 max_lon = 21
+
+#Inclination angle
 inc_angle = 65
 
 data_fetching(start_date, end_date, username, password, max_lat, min_lat, max_lon, min_lon, inc_angle, name)
